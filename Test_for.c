3d1d5623143c95025f781823ed39d651e713f169
nikifori <57923121+nikifori@users.noreply.github.com>
2019-11-20 00:19:21 +0200

    Add files via upload

---
+++ Test_for.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	for ( int i=0 ; i<5 ; i++)
+       	{
+            	printf("%d\n",i);
+       	}
+
+	printf("\nArxitektoniki\n\n");
+	
+	for (int i=0 ; i<5 ; i++)
+	{
+		printf("%d\n",i*i);
+	}
+
+  return 0;
+}
